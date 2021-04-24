da1cab83beae86c3eb560db249f110fd499bfe3a
Franziska Fieke <grenzbotin@googlemail.com>
2021-04-24 10:32:08 +0200

    Site setup: Favicons

--- components/HtmlHeader.tsx
+++ components/HtmlHeader.tsx
@@ -49,9 +49,24 @@ const HtmlHeader: React.FC<Props> = ({ title, description, keywords }) => {
 
       <meta name="msapplication-navbutton-color" content="#3A808A" />
       <meta name="msapplication-TileColor" content="#F6F0EC" />
+      <meta name="msapplication-TileImage" content="/site/ms-icon-144x144.png" />
 
-      <link rel="icon" href={`${publicUrl}/favicon.ico`} />
-      <link href={`${publicUrl}/manifest.json`} rel="manifest" />
+      <link rel="icon" href="./favicon.ico" />
+      <link rel="apple-touch-icon" sizes="57x57" href="/site/apple-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="60x60" href="/site/apple-icon-60x60.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/site/apple-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/site/apple-icon-76x76.png" />
+      <link rel="apple-touch-icon" sizes="114x114" href="/site/apple-icon-114x114.png" />
+      <link rel="apple-touch-icon" sizes="120x120" href="/site/apple-icon-120x120.png" />
+      <link rel="apple-touch-icon" sizes="144x144" href="/site/apple-icon-144x144.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/site/apple-icon-152x152.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/site/apple-icon-180x180.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/site/android-icon-192x192.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/site/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/site/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/site/favicon-16x16.png" />
+
+      <link rel="manifest" href="/site/manifest.json" />
     </Head>
   );
 };
