fd24df4569bcf0fd9c79db39987770b9463da27c
Franziska Fieke <grenzbotin@googlemail.com>
2021-04-24 10:48:52 +0200

    use base path

--- components/HtmlHeader.tsx
+++ components/HtmlHeader.tsx
@@ -1,8 +1,6 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 
-const publicUrl = "https://grenzbotin.github.io/decisionTracer";
-
 interface Props {
   title: string;
   description: string;
@@ -11,6 +9,9 @@ interface Props {
 
 const HtmlHeader: React.FC<Props> = ({ title, description, keywords }) => {
   const { asPath } = useRouter();
+
+  // on github page, the site is available via /decisionTracer
+  const basePath = asPath.includes("decisionTracer") ? "./decisionTracer" : ".";
 
   return (
     <Head>
@@ -22,8 +23,8 @@ const HtmlHeader: React.FC<Props> = ({ title, description, keywords }) => {
       <meta name="robots" content="noindex" />
 
       <meta name="twitter:card" content="summary" key="twcard" />
-      <meta property="og:url" content={`${publicUrl}${asPath}`} key="ogurl" />
-      <meta property="og:image" content={`${publicUrl}/images/og.png`} key="ogimage" />
+      <meta property="og:url" content={`${basePath}${asPath}`} key="ogurl" />
+      <meta property="og:image" content={`${basePath}/images/og.png`} key="ogimage" />
       <meta property="og:title" content={title} key="ogtitle" />
       <meta property="og:description" content={description} key="ogdesc" />
       <meta property="og:site_name" content="Rational Decision" key="ogsitename" />
@@ -49,24 +50,24 @@ const HtmlHeader: React.FC<Props> = ({ title, description, keywords }) => {
 
       <meta name="msapplication-navbutton-color" content="#3A808A" />
       <meta name="msapplication-TileColor" content="#F6F0EC" />
-      <meta name="msapplication-TileImage" content="/site/ms-icon-144x144.png" />
+      <meta name="msapplication-TileImage" content={`${basePath}/site/ms-icon-144x144.png`} />
 
-      <link rel="icon" href="./favicon.ico" />
-      <link rel="apple-touch-icon" sizes="57x57" href="/site/apple-icon-57x57.png" />
-      <link rel="apple-touch-icon" sizes="60x60" href="/site/apple-icon-60x60.png" />
-      <link rel="apple-touch-icon" sizes="72x72" href="/site/apple-icon-72x72.png" />
-      <link rel="apple-touch-icon" sizes="76x76" href="/site/apple-icon-76x76.png" />
-      <link rel="apple-touch-icon" sizes="114x114" href="/site/apple-icon-114x114.png" />
-      <link rel="apple-touch-icon" sizes="120x120" href="/site/apple-icon-120x120.png" />
-      <link rel="apple-touch-icon" sizes="144x144" href="/site/apple-icon-144x144.png" />
-      <link rel="apple-touch-icon" sizes="152x152" href="/site/apple-icon-152x152.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/site/apple-icon-180x180.png" />
-      <link rel="icon" type="image/png" sizes="192x192" href="/site/android-icon-192x192.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/site/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="96x96" href="/site/favicon-96x96.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/site/favicon-16x16.png" />
+      <link rel="icon" href={`${basePath}/site/favicon.ico`} />
+      <link rel="apple-touch-icon" sizes="57x57" href={`${basePath}/site/apple-icon-57x57.png`} />
+      <link rel="apple-touch-icon" sizes="60x60" href={`${basePath}/site/apple-icon-72x72.png`} />
+      <link rel="apple-touch-icon" sizes="72x72" href={`${basePath}/site/apple-icon-72x72.png`} />
+      <link rel="apple-touch-icon" sizes="76x76" href={`${basePath}/site/apple-icon-76x76.png`} />
+      <link rel="apple-touch-icon" sizes="114x114" href={`${basePath}/site/apple-icon-114x114.png`} />
+      <link rel="apple-touch-icon" sizes="120x120" href={`${basePath}/apple-icon-120x120.png`} />
+      <link rel="apple-touch-icon" sizes="144x144" href={`${basePath}/site/apple-icon-144x144.png`} />
+      <link rel="apple-touch-icon" sizes="152x152" href={`${basePath}/site/apple-icon-152x152.png`} />
+      <link rel="apple-touch-icon" sizes="180x180" href={`${basePath}/site/apple-icon-180x180.png`} />
+      <link rel="icon" type="image/png" sizes="192x192" href={`${basePath}/site/android-icon-192x192.png`} />
+      <link rel="icon" type="image/png" sizes="32x32" href={`${basePath}/site/favicon-32x32.png`} />
+      <link rel="icon" type="image/png" sizes="96x96" href={`${basePath}/site/favicon-96x96.png`} />
+      <link rel="icon" type="image/png" sizes="16x16" href={`${basePath}/site/favicon-16x16.png`} />
 
-      <link rel="manifest" href="/site/manifest.json" />
+      <link rel="manifest" href={`${basePath}/site/manifest.json`} />
     </Head>
   );
 };
